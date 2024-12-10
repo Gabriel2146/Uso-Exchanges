cbe400550cb6f7b822d5a3b0dddc52799c1d715e
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2024-12-10 12:49:41 -0500

    abstraccion en Producer_direct

--- queue_system/producers/producer_direct.py
+++ queue_system/producers/producer_direct.py
@@ -1,22 +1,14 @@
-import pika
+from brokers.rabbitmq_broker import RabbitMQBroker
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-
-    # Declare the direct exchange
-    channel.exchange_declare(exchange='notifications_direct', exchange_type='direct')
-
+    broker = RabbitMQBroker()
     messages = [
         ("email", "Direct Notification: Email message content."),
         ("sms", "Direct Notification: SMS message content."),
     ]
 
     for routing_key, message in messages:
-        channel.basic_publish(exchange='notifications_direct', routing_key=routing_key, body=message)
-        print(f"Sent: {message} with routing key: {routing_key}")
-
-    connection.close()
+        broker.publish(exchange='notifications_direct', routing_key=routing_key, message=message)
 
 if __name__ == "__main__":
     main()
