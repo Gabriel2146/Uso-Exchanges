19de2335d2fab050628ce123d0706be4676e7f77
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2024-12-10 12:52:09 -0500

    abstraccion consumer_email e inity.Py

--- queue_system/Consumers/consumer_email.py
+++ queue_system/Consumers/consumer_email.py
@@ -1,24 +1,11 @@
-import pika
+from brokers.rabbitmq_broker import RabbitMQBroker
 
 def callback(ch, method, properties, body):
     print(f"Received email notification: {body.decode()}")
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-
-    # Declare the direct exchange
-    channel.exchange_declare(exchange='notifications_direct', exchange_type='direct')
-
-    # Declare a queue and bind it to the exchange with the routing key 'email'
-    queue_name = 'email_notifications'
-    channel.queue_declare(queue=queue_name)
-    channel.queue_bind(exchange='notifications_direct', queue=queue_name, routing_key='email')
-
-    print("Waiting for email notifications. To exit press CTRL+C")
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-    channel.start_consuming()
+    broker = RabbitMQBroker()
+    broker.consume(exchange='notifications_direct', queue='email_notifications', routing_key='email', callback=callback)
 
 if __name__ == "__main__":
     main()
