dad4fe4e1f490e1b3acbb2b37ec22f3774acc8f3
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2024-12-10 12:55:28 -0500

    interfaz abstracta para el broker.

--- queue_system/producers/producer_direct.py
+++ queue_system/producers/producer_direct.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+# Agregar el directorio raíz al sys.path si no se reconoce automáticamente
+root_dir = Path(__file__).resolve().parents[1]
+sys.path.append(str(root_dir))
+
 from brokers.rabbitmq_broker import RabbitMQBroker
 
 def main():
