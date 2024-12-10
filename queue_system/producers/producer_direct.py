fe4e151012ba46b7aca5356ba34f089d457ae6df
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2024-12-10 12:56:54 -0500

    Productor para mensajes directos.

--- queue_system/producers/producer_direct.py
+++ queue_system/producers/producer_direct.py
@@ -1,10 +1,3 @@
-import sys
-from pathlib import Path
-
-# Agregar el directorio raíz al sys.path si no se reconoce automáticamente
-root_dir = Path(__file__).resolve().parents[1]
-sys.path.append(str(root_dir))
-
 from brokers.rabbitmq_broker import RabbitMQBroker
 
 def main():
